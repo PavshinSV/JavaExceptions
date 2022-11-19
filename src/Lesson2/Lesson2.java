1714757b0d787a341978e04c4875d1f51520cb76
PSV <pavshin@mail.ru>
2022-11-20 00:58:06 +0700

    task 2,3 from lesson2 is finished

--- src/Lesson2/Lesson2.java
+++ src/Lesson2/Lesson2.java
@@ -6,11 +6,9 @@ package Lesson2;
 //        необходимо повторно запросить у пользователя ввод данных.
 //        2. Если необходимо, исправьте данный код (задание 2
 //        https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
-//
-//        2.1 Дан следующий код, исправьте его там, где требуется (задание 3
+//        3 Дан следующий код, исправьте его там, где требуется (задание 3
 //        https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
-//
-//        2.2 Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
+//        4 Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
 //        Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
 
 
@@ -19,14 +17,27 @@ import java.util.Scanner;
 
 public class Lesson2 {
     static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Optional<Float> fl = inputFloat();
-        while (fl.isEmpty()){
-            System.out.println("Try again!");
-            fl=inputFloat();
+//  Код к заданию 1
+//        Optional<Float> fl = inputFloat();
+//        while (fl.isEmpty()){
+//            System.out.println("Try again!");
+//            fl=inputFloat();
+//        }
+//        System.out.println("You entered float number: "+fl.get());
+//        sc.close();
+
+// Код к заданию 2
+        int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        try {
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
         }
-        System.out.println("You entered float number: "+fl.get());
-        sc.close();
+
     }
 
     public static Optional inputFloat() {
